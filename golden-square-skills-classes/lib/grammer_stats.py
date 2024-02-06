f0f9323851f828647c1cb1da260e576fad3c85f7
Amy Brown <amy.brown@spektrix.com>
2024-02-06 12:05:02 +0000

    Grammer Stats tested and passing

--- golden-square-skills-classes/lib/grammer_stats.py
+++ golden-square-skills-classes/lib/grammer_stats.py
@@ -1,17 +1,16 @@
 class GrammarStats:
     def __init__(self):
-        pass
+        self.check_true = 0
+        self.check_false = 0
 
     def check(self, text):
-        # Parameters:
-        #   text: string
-        # Returns:
-        #   bool: true if the text begins with a capital letter and ends with a
-        #         sentence-ending punctuation mark, false otherwise
-        pass
+        if (text[-1] in ".!?") and (text[0].isupper()):
+            self.check_true += 1
+            return True
+        else:
+            self.check_false += 1
+            return False
 
     def percentage_good(self):
-        # Returns:
-        #   int: the percentage of texts checked so far that passed the check
-        #        defined in the `check` method. The number 55 represents 55%.
-        pass
+        total_checked = (self.check_false+self.check_true)
+        return int((self.check_true /total_checked)*100)
